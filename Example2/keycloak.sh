e02a8d8083a7924daf7914d33d34440b7786cc3b
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-08 23:05:43 -0300

    Create keycloak dockerized for oauth projects

---
+++ Example2/keycloak.sh
@@ -0,0 +1,3 @@
+## https://www.keycloak.org/getting-started/getting-started-docker
+
+docker run -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:26.0.0 start-dev
